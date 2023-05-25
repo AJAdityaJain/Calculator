0c8f87d067d0f2433f53f604dc26bf2712aa2a09
AJAdityaJain <adityajain121108@gmail.com>
2023-05-25 13:59:15 +0530

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public string AskOp()
+        {
+            string op = "";
+            bool opError = true;
+
+            Console.WriteLine("What is the operation that needs to be performed.");
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("$ Addition $ -->  (\"+\") ", Console.BackgroundColor, Console.ForegroundColor);
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("$ Subtraction $ -->  (\"-\") ", Console.BackgroundColor, Console.ForegroundColor);
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("$ Multiplication $ -->  (\"*\") ", Console.BackgroundColor, Console.ForegroundColor);
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("$ Division $ -->  (\"/\") ", Console.BackgroundColor, Console.ForegroundColor);
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("$ Power Of $ -->  (\"^\") ", Console.BackgroundColor, Console.ForegroundColor);
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            while (opError)
+            {
+
+                op = Console.ReadLine();
+                Console.Beep();
+
+                if (op!="+" && op != "-" && op != "*" && op != "/" && op != "^")
+                {
+                    Console.WriteLine("Try with a given operator >:[");
+                    opError = true;
+                    Console.Beep();
+                }
+                else
+                {
+                    opError = false;
+                }
+
+            }
+            
+            return op;
+        }
+
+        public double AskNum(string position)
+        {
+            string strnum;
+            bool parseError = true;
+            double num = 0;
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("What is the "+position+" term", Console.BackgroundColor, Console.ForegroundColor);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            strnum = Console.ReadLine();
+
+            Console.Beep();
+            Console.WriteLine("");
+
+            while (parseError)
+            {
+                if (Double.TryParse(strnum, out num) == true)
+                {
+                    num = Double.Parse(strnum);
+                    parseError = false;
+                }
+                else
+                {
+                    Console.WriteLine("This time try with an actual number ( -_-*)");
+                    strnum = Console.ReadLine();
+                    Console.Beep();
+                    parseError = true;
+                }
+            }
+
+            return num;
+        }
+
+        public double Calulate(double num1, double num2, string op)
+        {
+            double answer = 0;
+
+            switch (op)
+            {
+                case "+":
+                    answer = num1 + num2;
+                    break;
+
+                case "-":
+                    answer = num1 - num2;
+                    break;
+
+                case "*":
+                    answer = num1 * num2;
+                    break;
+
+                case "/":
+                    answer = num1 / num2;
+                    break;
+
+                case "^":
+                    answer = Math.Pow(num1, num2);
+                    break;
+
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+
+                Calculator Maths = new Calculator();
+
+                string operators = Maths.AskOp();
+                double num1 = Maths.AskNum("first");
+                double num2 = Maths.AskNum("second");
+                double answer = Maths.Calulate(num1, num2, operators);
+                string strnum1 = Convert.ToString(num1);
+                string strnum2 = Convert.ToString(num2);
+                string y_n;
+
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(strnum1 + operators + strnum2 + '=');
+                
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(answer);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("If you want to continue type (y)es otherwise (n)o..");
+                Console.Beep();
+                y_n = Console.ReadLine();
+                
+                if(y_n == "y" || y_n == "yes")
+                {
+                    repeat = true;
+                    Console.Beep();
+                    Console.Clear();
+                }
+                else
+                {
+                    repeat = false;
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
